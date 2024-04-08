b1e5f4c419d93e2818b2724004f91ca26fa939b5
Dmitry Shapovalov <shenf@mail.ru>
2024-04-08 22:37:33 +0300

    Задача imagemagic

---
+++ src/main/java/ru/d_shap/plugin/imagemagick/ImageMagickGradleAction.java
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// ImageMagick Gradle Plugin is a plugin to call ImageMagick CLI.
+// Copyright (C) 2024 Dmitry Shapovalov.
+//
+// This file is part of ImageMagick Gradle Plugin.
+//
+// ImageMagick Gradle Plugin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ImageMagick Gradle Plugin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.plugin.imagemagick;
+
+import org.gradle.api.Action;
+import org.gradle.api.Task;
+
+/**
+ * ImageMagick Gradle Action.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class ImageMagickGradleAction implements Action<Task> {
+
+    /**
+     * Create new object.
+     */
+    public ImageMagickGradleAction() {
+        super();
+    }
+
+    @Override
+    public void execute(final Task task) {
+        System.out.println("ImageMagick!");
+    }
+
+}
