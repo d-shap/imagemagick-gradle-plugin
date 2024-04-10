218c7126e950aadcfc496e0956441f691dfa587a
Dmitry Shapovalov <shenf@mail.ru>
2024-04-10 16:52:28 +0300

    Конфигурация параметров

--- src/main/java/ru/d_shap/gradle/plugin/imagemagick/configuration/ParameterConfiguration.java
+++ src/main/java/ru/d_shap/gradle/plugin/imagemagick/configuration/ParameterConfiguration.java
@@ -17,9 +17,15 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-package ru.d_shap.gradle.plugin.imagemagick.extension;
+package ru.d_shap.gradle.plugin.imagemagick.configuration;
 
-import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.d_shap.gradle.plugin.imagemagick.parameters.DestinationFileParameter;
+import ru.d_shap.gradle.plugin.imagemagick.parameters.OptionParameter;
+import ru.d_shap.gradle.plugin.imagemagick.parameters.Parameter;
+import ru.d_shap.gradle.plugin.imagemagick.parameters.SourceFileParameter;
 
 /**
  * The parameter configuration.
@@ -28,22 +34,50 @@ import java.io.PrintStream;
  */
 public class ParameterConfiguration {
 
+    private final List<Parameter> _parameters;
+
     /**
      * Create new object.
      */
     public ParameterConfiguration() {
         super();
+        _parameters = new ArrayList<>();
     }
 
     /**
-     * Add a parameter.
+     * Get the parameters.
+     *
+     * @return the parameters.
+     */
+    public List<Parameter> getParameters() {
+        return _parameters;
+    }
+
+    /**
+     * Add the source file parameter.
+     */
+    public void sourceFile() {
+        Parameter parameter = new SourceFileParameter();
+        _parameters.add(parameter);
+    }
+
+    /**
+     * Add the destination file parameter.
+     */
+    public void destinationFile() {
+        Parameter parameter = new DestinationFileParameter();
+        _parameters.add(parameter);
+    }
+
+    /**
+     * Add an arbitrary parameter.
      *
      * @param name the name.
      * @param args the args.
      */
-    public void methodMissing(final String name, final Object... args) {
-        PrintStream printStream = System.out;
-        printStream.println("!!!" + name + "!!!");
+    public void methodMissing(final String name, final Object args) {
+        Parameter parameter = new OptionParameter(name, args);
+        _parameters.add(parameter);
     }
 
 }
